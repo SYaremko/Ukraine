775cba0df86ffee918d4f8ecdea1997140c3962a
Solomiia Yaremko <19solyaremko@gmail.com>
2019-05-12 19:56:49 +0300

    delete

--- src/main/java/ua/com/ushop/dao/CartDAO.java
+++ src/main/java/ua/com/ushop/dao/CartDAO.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.Query;
 import ua.com.ushop.entity.Cart;
 import java.util.List;
 
-
 public interface CartDAO extends JpaRepository <Cart, Integer> {
 
     @Query("from Cart c join fetch c.products")
